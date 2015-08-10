a1b0f79d657da64f063453922a17f5cbf3020a35
Jeff Sternberg <jeffsternberg@gmail.com>
2015-08-10 10:20:45 -0400

    flake8 formatting

--- wordpress/urls.py
+++ wordpress/urls.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.conf.urls import patterns, url
 
 
-urlpatterns = patterns('wordpress.views',
-    url(r'^load_post$', 'load_post_webhook', name='wp_load_post_webhook'),
+urlpatterns = patterns(
+    "wordpress.views",
+    url(r"^load_post$", "load_post_webhook", name="wp_rest_load_post_webhook"),
 )
-
